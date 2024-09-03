78c3fca0ec896b616439617fc092cb8ca9db92f2
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-09-03 20:48:21 +0530

    Minor UI changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
   const handleSignOut = () => {
     signOut(auth).then(() => {
-      dispatch()
       navigate("/");
     }).catch((error) => {
       console.error(error.message);
@@ -22,9 +21,9 @@ const Header = () => {
         alt="header logo"
         className="w-44 my-2"
       />
-      <div>
+      {user && <div>
         <button onClick={handleSignOut} className="bg-red-600 px-2 py-1 rounded-md text-white font-semibold">Sign out</button>
-      </div>
+      </div>}
     </div>
   )
 }
